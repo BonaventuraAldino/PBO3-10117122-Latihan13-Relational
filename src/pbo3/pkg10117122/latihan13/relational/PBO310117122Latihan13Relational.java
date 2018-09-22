8f2df66a5a0f3efe7373af507c6a9f83c916a530
BonaventuraAldino <aldysenda99@gmail.com>
2018-09-23 01:47:22 +0700

    PBO3-10117122-Latihan13-Relational

---
+++ src/pbo3/pkg10117122/latihan13/relational/PBO310117122Latihan13Relational.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117122.latihan13.relational;
+
+/**
+ *
+ * @author lenovo
+ * 
+ * NAMA  : BONAVENTURA ALDINO SENDA SENI
+ * KELAS : PBO-3
+ * NIM   : 10117122
+ * 
+ */
+public class PBO310117122Latihan13Relational {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int bil1 = 10;
+        int bil2 = 20;
+        System.out.println("a == b = " + (bil1 + bil2));
+        System.out.println("a != b = " + (bil1 + bil2));
+        System.out.println("a > b = " + (bil1 > bil2));
+        System.out.println("a < b = " + (bil1 < bil2));
+        System.out.println("b >= a = " + (bil2 >= bil1));
+        System.out.println("b <= a = " + (bil2 <= bil1));
+    }
+}
